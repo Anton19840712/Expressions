5498f4ce7b8c9fead72ead05f92e4ca47976402a
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-11-01 01:50:28 +0300

    Change migrations.

---
+++ Beyond-LINQ/Beyond-LINQ-Using-Expression-Trees-in-.NET.React/Data/EntitiesDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Beyond_LINQ_Using_Expression_Trees_in_.NET.React.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Beyond_LINQ_Using_Expression_Trees_in_.NET.React.Data
+{
+    public class EntitiesDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+        public DbSet<Category> Categories { get; set; }
+
+        public EntitiesDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
