4440bf7165e646dbe34d3e1f458dca0193cf12dd
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-11-01 01:51:58 +0300

    Add some changes in product controller.

--- Beyond-LINQ/Beyond-LINQ-Using-Expression-Trees-in-.NET.React/Controllers/ProductsController.cs
+++ Beyond-LINQ/Beyond-LINQ-Using-Expression-Trees-in-.NET.React/Controllers/ProductsController.cs
@@ -13,29 +13,27 @@ namespace Beyond_LINQ_Using_Expression_Trees_in_.NET.React.Controllers
     [Route("api/[controller]")]
     public class ProductsController: Controller
     {
-        private readonly ApplicationDbContext _dbContext;
+        public static readonly Spec<Product> IsForSaleSpec = new Spec<Product>(x=>x.IsForSale);
+        public static readonly Spec<Product> IsInStockSpec = new Spec<Product>(x=>x.InStock>0);
+
+        private readonly EntitiesDbContext _dbContext;
         private readonly IMapper _mapper;
 
-        public ProductsController(ApplicationDbContext dbContext, IMapper mapper)
+        public ProductsController(EntitiesDbContext dbContext, IMapper mapper)
         {
             _dbContext = dbContext;
             _mapper = mapper;
         }
 
         [HttpGet]
-        public IActionResult Get([FromServices] DbContext dbContext,
-            [FromQuery] ProductFilter productFilter,
-            [FromQuery] string orderBy)
+        public IActionResult Get([FromServices] DbContext dbContext, [FromQuery] ProductFilter productFilter, [FromQuery] string orderBy)
         {
             var spec = SpecBuilder<ProductListDto>.Build(productFilter);
 
-            // _dbContext.Products.Select(x =>
-            //     new ProductListDto{
-            //         Id = x.Id,
-            //         Name = x.Name,
-            //         CategoryName = x.Category.Name
-            //     });
-            
+            //dbContext.Products
+            //    .Where(Product.IsForSaleSpec || Product.IsInStockSpec)
+            //    .ToList();
+
             var q = _mapper
                 .ProjectTo<ProductListDto>(_dbContext.Products)
                 .Where(spec);
